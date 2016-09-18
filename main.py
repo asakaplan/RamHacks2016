cebaae01b462dceb903e9e2cde906f0f649189d6
CarlotzTech-Charlie <cbradshaw@carlotz.com>
2016-09-18 13:10:47 -0400

    Implemented post of data

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ import os
 import urllib2
 import urllib
 import re
-
+import requests
+import json
  
 
 
 
 
 mainUrl = "https://s3.amazonaws.com/elephant-ramhacks-dev/"
+postUrl = "https://ramhacks-2016.herokuapp.com/certifies"
 response = urllib2.urlopen(mainUrl)
 html = response.read()
 root = [a for a in re.findall("<Key>(.*?)<", html)]
-
-
+outputDict = {}
 for i in range(len(root)-1,-1,-1):
 	if root[i].endswith("/"):
 		root.pop(i)
@@ -65,8 +66,22 @@ for mainImgName in  listOfFiles:
 			maxFileIndex = i
 	smallImages.pop(maxFileIndex)
 	print listOfTemplateFiles[maxFileIndex] + " is a subimage of " + mainImgName
+	outputDict[listOfTemplateFiles[maxFileIndex][:listOfTemplateFiles[maxFileIndex].find(".")]] = mainImgName[:mainImgName.find(".")]
 	listOfTemplateFiles.pop(maxFileIndex)
 	maxNum = 0
 	maxFileIndex = 0
+
+print outputDict
+
+payload = {	'env': 'dev',
+			'answers': outputDict}
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(postUrl, data=json.dumps(payload), headers=headers)
+
+for element in response:
+	print element
+
+
 
 
