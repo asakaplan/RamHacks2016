9ab591fe5faed84b5fc6b3c851bbfa840109c3d5
CarlotzTech-Charlie <cbradshaw@carlotz.com>
2016-09-18 12:39:41 -0400

    Main.py now downloads files

--- main.py
+++ main.py
@@ -4,18 +4,42 @@ import imutils
 import glob
 import cv2
 import os
+import urllib2
+import urllib
+import re
+
  
-# construct the argument parser and parse the arguments
-"""
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--template", required=True, help="Path to template image")
-ap.add_argument("-i", "--images", required=True,
-	help="Path to images where template will be matched")
-ap.add_argument("-v", "--visualize",
-	help="Flag indicating whether or not to visualize each iteration")
-args = vars(ap.parse_args())
- """
-# load the image image, convert it to grayscale, and detect edges
+
+
+
+
+mainUrl = "https://s3.amazonaws.com/elephant-ramhacks-dev/"
+response = urllib2.urlopen(mainUrl)
+html = response.read()
+root = [a for a in re.findall("<Key>(.*?)<", html)]
+
+
+for i in range(len(root)-1,-1,-1):
+	if root[i].endswith("/"):
+		root.pop(i)
+
+
+
+
+
+for element in root:
+	target_folder, ignore = element.split("/",1)
+
+	if not os.path.exists(target_folder):
+		os.makedirs(target_folder)
+	urllib.urlretrieve(mainUrl+element, element)
+
+
+
+
+
+
+
 listOfFiles = [filename for filename in os.listdir('source') if filename.endswith('jpg')]
 listOfTemplateFiles = [filename for filename in os.listdir('targets') if filename.endswith('png')]
 maxFileName = ""
@@ -26,33 +50,6 @@ smallImages = [cv2.imread("targets/" + filename) for filename in listOfTemplateF
 smallImages = map(lambda a: cv2.cvtColor(a, cv2.COLOR_BGR2GRAY), smallImages)
 smallImages = map(lambda a: cv2.Canny(a, 50, 200), smallImages)
 
-
-
-
-"""
-for filename in os.listdir('targets'):
-	if filename.endswith('png'):
-		template = cv2.imread("targets/" + filename)
-		template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-		template = cv2.Canny(template, 50, 200)
-		#(tH, tW) = template.shape[:2]
-		cv2.imshow("Template", template)
-		cv2.waitKey()
-
-		for mainImgName in  listOfFiles:
-			mainImg = cv2.imread("source/" + mainImgName)
-			mainImg = cv2.cvtColor(mainImg, cv2.COLOR_BGR2GRAY)
-			mainImg = cv2.Canny(mainImg, 50, 200)
-			#(tH, tW) = mainImg.shape[:2]
-			result = cv2.matchTemplate(mainImg, template, cv2.TM_CCOEFF)
-			(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
-			if(maxVal > maxNum):
-				maxNum = maxVal
-				maxFileName = mainImgName
-		listOfFiles.remove(maxFileName)
-		print filename + " is a subimage of " + maxFileName
-		maxNum = 0
-"""
 
 for mainImgName in  listOfFiles:
 	mainImg = cv2.imread("source/" + mainImgName)
@@ -73,55 +70,3 @@ for mainImgName in  listOfFiles:
 	maxFileIndex = 0
 
 
-
-"""
-# loop over the images to find the template in
-for imagePath in glob.glob(args["images"] + "/*.jpg"):
-	# load the image, convert it to grayscale, and initialize the
-	# bookkeeping variable to keep track of the matched region
-	image = cv2.imread(imagePath)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	found = None
- 
-	# loop over the scales of the image
-	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-		# resize the image according to the scale, and keep track
-		# of the ratio of the resizing
-		resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
-		r = gray.shape[1] / float(resized.shape[1])
- 
-		# if the resized image is smaller than the template, then break
-		# from the loop
-		if resized.shape[0] < tH or resized.shape[1] < tW:
-			break
-		# detect edges in the resized, grayscale image and apply template
-		# matching to find the template in the image
-		edged = cv2.Canny(resized, 50, 200)
-		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
- 
-		# check to see if the iteration should be visualized
-		if args.get("visualize", False):
-			# draw a bounding box around the detected region
-			clone = np.dstack([edged, edged, edged])
-			cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-				(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-			cv2.imshow("Visualize", clone)
-			cv2.waitKey(0)
- 
-		# if we have found a new maximum correlation value, then ipdate
-		# the bookkeeping variable
-		if found is None or maxVal > found[0]:
-			found = (maxVal, maxLoc, r)
- 
-	# unpack the bookkeeping varaible and compute the (x, y) coordinates
-	# of the bounding box based on the resized ratio
-	(_, maxLoc, r) = found
-	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
- 
-	# draw a bounding box around the detected result and display the image
-	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
-"""
